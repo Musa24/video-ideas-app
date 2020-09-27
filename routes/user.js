b66b264ab8ed2b9a6975b7e5c6f5944b71f685af
MUSA SALUMU MUHOZYA <musa24@MacBook-Pro-MUSA.local>
2020-09-28 00:00:26 +0300

    Register form validation

--- routes/user.js
+++ routes/user.js
@@ -11,6 +11,31 @@ router.get('/login', (req, res) => {
   res.render('users/login');
 });
 
-//Processing register
+//Processing register request
+router.post('/register', (req, res) => {
+  const { firstName, lastName, email, password, password1 } = req.body;
+  //Validation
+  let errors = [];
+  if (password !== password1) {
+    errors.push({ text: 'Password doe not match' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ text: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('users/register', {
+      firstName,
+      lastName,
+      email,
+      password1,
+      password,
+      errors,
+    });
+  } else {
+    res.send('Register');
+  }
+});
 
 module.exports = router;
